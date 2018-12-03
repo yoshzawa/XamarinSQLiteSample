e2441bd2fc416afafe0a04fc7470418db5e906ac
t.yoshizawa <t.yoshizawa@jc-21.jp>
2018-12-03 14:38:44 +0900

    LocationItem追加

---
+++ FormsSQLite/FormsSQLite/LocationItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace FormsSQLite
+{
+    public class LocationItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
