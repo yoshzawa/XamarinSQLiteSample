83a1ce8b030b8cb8d6ae208fd23a0283c22cff6a
t.yoshizawa <t.yoshizawa@jc-21.jp>
2018-12-03 15:17:39 +0900

    ボタンを押すと保存できるようにした

--- FormsSQLite/FormsSQLite/MainPage.xaml.cs
+++ FormsSQLite/FormsSQLite/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using FormsSQLite;
 
 namespace FormsSQLite
 {
@@ -12,6 +13,43 @@ namespace FormsSQLite
         public MainPage()
         {
             InitializeComponent();
+            btn1.Clicked += Btn1clicked;
+            btn2.Clicked += Btn2clicked;
+            btnView.Clicked += BtnVWclicked;
+
+        }
+
+        private void Btn1clicked(object sender, EventArgs e)
+        {
+            LocationItem item1 = new LocationItem()
+            {
+                Latitude = 37.7540613F,
+                Longitude = 140.4682551F,
+                Name = "キッチンカロリー"
+            };
+            Save(item1);
+        }
+
+        private async void Save(LocationItem item1)
+        {
+            await App.Database.SaveItemAsync(item1);
+            await DisplayAlert("DATA1","RECORDED","OK");
+        }
+
+        private void Btn2clicked(object sender, EventArgs e)
+        {
+            LocationItem item2 = new LocationItem()
+            {
+                Latitude = 38.2192076F,
+                Longitude = 140.868182F,
+                Name = "ビストロ アンプル"
+            };
+            Save(item2);
+        }
+
+        private void BtnVWclicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
