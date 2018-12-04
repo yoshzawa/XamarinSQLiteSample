8183bbe7e38e2cb94e53fcceb2a9f093b004814e
t.yoshizawa <t.yoshizawa@jc-21.jp>
2018-12-04 11:15:38 +0900

    結果表示ページ完成

--- FormsSQLite/FormsSQLite/ResultPage.xaml.cs
+++ FormsSQLite/FormsSQLite/ResultPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using FormsSQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,31 @@ namespace FormsSQLite
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected  override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var result = await App.Database.GetItemsAsync();
+
+            int size = result.Count;
+            await DisplayAlert("ResultPage", "record=" + size , "OK");
+
+            var layout2 = new StackLayout() { Spacing = 10 };
+
+            foreach(var location in result)
+            {
+                var layout3 = new StackLayout() ;
+                layout3.Children.Add(new Label() { Text = "name:" + location.Name });
+                layout3.Children.Add(new Label() { Text = "Latitude:" + location.Latitude });
+                layout3.Children.Add(new Label() { Text = "Longitude:" + location.Longitude });
+                layout2.Children.Add(layout3);
+            }
+
+            layout.Children.Add( layout2);
+
+
+        }
+    }
+
+
 }
